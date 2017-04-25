28b74df2d517c89d240054d1afcb29b41a466a44
sixiaoyou <yunshaliying@sina.com>
2017-04-25 08:19:22 +0800

    Add RotateArray

---
+++ src/main/java/pers/you/algorithm/array/RotateArray.java
@@ -0,0 +1,48 @@
+package pers.you.algorithm.array;
+
+import org.junit.Test;
+
+public class RotateArray {
+    public void swap(int[] array,int a,int b){
+        int t=array[a];
+        array[a]=array[b];
+        array[b]=t;
+    }
+    
+    public void printArray(int[] array){
+        for(int i:array){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    
+    public void reverse(int[] array,int start,int end){
+        if(array==null||array.length<=1){
+            return;
+        }
+        for(int i=start,j=end;i<j;i++,j--){
+            swap(array, i, j);;
+        }
+    }
+    
+    public void rotate(int[] nums,int k){
+        if(k==0){
+            return;
+        }
+        int n = nums.length;
+        if(k>n){
+            k=k%n;
+        }
+        reverse(nums, 0, n-1-k);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+    
+    @Test
+    public void test(){
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        printArray(nums);
+        rotate(nums, 4);
+        printArray(nums);
+    }
+}
